d5e867a750db35f5c1b44da2732448d54ad0c983
folajhimie <blackhemisphere12@gmail.com>
2023-05-09 06:48:46 +0100

    reworked the sign in page for now...

--- src/page/sign-in/SignIn.tsx
+++ src/page/sign-in/SignIn.tsx
@@ -49,73 +49,99 @@ const SignUp = () => {
               </div>
             </div>
           </div>
-          <div className='flex md:flex-col md:justify-center  bg-[#F4F7FA] px-10 xs:justify-center xs:flex-row md:items-stretch xs:items-center h-screen'>
-            <form
-              className="border rounded-sm p-7 shadow-lg bg-white  mx-4"
-              method="POST"
-            >
-              <div className="">
+          <div className="flex bg-[#F4F7FA] md:flex-1 min-h-screen flex-col w-full  justify-center items-center relative z-50">
+            <div className="flex  items-center justify-center h-screen  w-full">
+              <div className="w-full flex flex-col p-5 max-w-lg">
 
-                <div className="flex">
-                  <input
-                    type="email"
-                    name="email"
-                    id="email"
-                    className="w-full pl-3 pr-3 py-3 border rounded-sm border-gray-200 outline-none focus:border-[#61297F] text-sm"
-                    placeholder="Email Address"
-                    required
-                  />
-                </div>
-              </div>
 
-              <div className="mt-5 w-full">
+                <form
+                  className="border rounded-sm p-7 shadow-lg bg-white  w-full flex-1 mt-4"
+                  method="POST"
+                >
+                  <div className="">
 
-                <div className="">
-                  <div className="flex">
-                    <input
-                      type="password"
-                      name="password"
-                      id="password"
-                      placeholder="Password"
-                      className="w-full pl-3 pr-3 py-3 rounded-sm border border-gray-200 outline-none focus:border-[#61297F] text-sm"
-                      autoComplete="off"
-                      required
-                    />
+                    <div className="flex">
+                      <input
+                        type="email"
+                        name="email"
+                        id="email"
+                        className="w-full pl-3 pr-3 py-3 border rounded-sm border-gray-200 outline-none focus:border-[#61297F] text-sm"
+                        placeholder="Email Address"
+                        required
+                      />
+                    </div>
+                  </div>
+
+                  <div className="mt-5 w-full">
+
+                    <div className="">
+                      <div className="flex">
+                        <input
+                          type="password"
+                          name="password"
+                          id="password"
+                          placeholder="Password"
+                          className="w-full pl-3 pr-3 py-3 rounded-sm border border-gray-200 outline-none focus:border-[#61297F] text-sm"
+                          autoComplete="off"
+                          required
+                        />
+
+                      </div>
+                    </div>
+                  </div>
+
+                  <div className="my-3">
+                    <Link
+                      to="/auth/forgot-password"
+                      className="text-xs font-medium text-[#61297F] my-6"
+                    >
+                      Forgot password?
+                    </Link>
 
                   </div>
+
+                  <div className="flex justify-between mt-8">
+                    <div className="flex justify-center items-center xs:flex-col md:flex-row">
+                      <span className="text-xs text-[#b8c3d2]">
+                        New user?
+                      </span>
+                      <span className="text-[#61297F] text-xs ml-2">
+                        Create account
+                      </span>
+                    </div>
+
+                    <div>
+                      <Link to="/">
+                        <button className="bg-[#61297F] py-2 rounded text-white px-6 text-xs">
+                          sign in
+                        </button>
+                      </Link>
+                    </div>
+                  </div>
+                </form>
+                <div className="flex justify-between mt-8">
+                  <div className="flex justify-center items-center xs:flex-col md:flex-row">
+                    <span className="text-xs text-[#b8c3d2]">
+                      Trouble signing in?
+                    </span>
+                    <span className="text-xs text-[#b8c3d2] ml-2">
+                      Chat with us
+                    </span>
+                  </div>
+
+                  <div>
+                    <span className="text-[#b8c3d2] text-xs ml-2">
+                      Privacy | Terms
+                    </span>
+                  </div>
                 </div>
-              </div>
-
-              <div className="my-3">
-                <Link
-                  to="/auth/forgot-password"
-                  className="text-xs font-medium text-[#61297F] my-6"
-                >
-                  Forgot password?
-                </Link>
 
               </div>
+            </div>
 
-              <div className="flex justify-between mt-8">
-                <div className="flex justify-center items-center xs:flex-col md:flex-row">
-                  <span className="text-xs text-[#b8c3d2]">
-                    New user?
-                  </span>
-                  <span className="text-[#61297F] text-xs ml-2">
-                    Create account
-                  </span>
-                </div>
 
-                <div>
-                  <Link to="/">
-                    <button className="bg-[#61297F] py-2 rounded text-white px-6">
-                      sign in
-                    </button>
-                  </Link>
-                </div>
-              </div>
-            </form>
           </div>
+
         </div>
       </div>
 
